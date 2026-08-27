c74a91490cf77ee3cea8d4b37cd73e0d9391a7e2
maker9100 <gang96108@gmail.com>
2026-08-27 09:45:30 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyChhloxx7gneGC4sywF_zSbjyhLuaEpkg8",
+  authDomain: "card-fortress-1740b.firebaseapp.com",
+  databaseURL: "https://card-fortress-1740b-default-rtdb.firebaseio.com",
+  projectId: "card-fortress-1740b",
+  storageBucket: "card-fortress-1740b.firebasestorage.app",
+  messagingSenderId: "909835952729",
+  appId: "1:909835952729:web:256c5e9eef071e19654748",
+  measurementId: "G-Q7QMKGXH95"
+};
